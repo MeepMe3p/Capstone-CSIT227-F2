c8366eabeb7348c18bcf36de71c5c7d7c74cd4ab
rayalindley <rayactrlf5@gmail.com>
2023-12-07 23:32:03 +0800

    Created the details for Job characters

--- src/Capstone.java
+++ src/Capstone.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -165,8 +167,13 @@ public class Capstone extends JFrame{
                 cardLayout.show(mainPanel,"InfoPanel");
                 //halu raya ikaw diri so like basicaly once maopen ni sha dapat naa nay mga sulod ang combobox pwede raka mag cbCharacter.addItem() para sa each
                 //now go to cbCharacter
+                cbCharacter.addItem("Priest");
+                cbCharacter.addItem("Knight");
+                cbCharacter.addItem("Mage");
+                cbCharacter.setSelectedIndex(-1);
             }
         });
+
         bSelect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -188,7 +195,39 @@ public class Capstone extends JFrame{
                 //halu raya okaeri nice to meet you again hahaha so like kani sha kay once maselect ang for example ang kanang ya kay ang text area kay maset sha
                 //into something depending sa iyang naselect, for now gahuna2 pajud ko if iusa ba nato ka text file each character or iseparate ba nato pwede ta
                 //magsabot unsay mas maayu
+                String selectedChar = (String) cbCharacter.getSelectedItem();
 
+                taInfo.setText("");
+
+
+                String symbol;
+
+                if(selectedChar != null) {
+                    String charDetails = "src/JobDetails";
+
+                    try {
+                        String details = Files.readString(Paths.get(charDetails));
+
+                        symbol = switch (selectedChar) {
+                            case "Priest" -> "!";
+                            case "Knight" -> "@";
+                            case "Mage" -> "#";
+                            default -> "";
+                        };
+
+                        int startSymbol = details.indexOf(symbol);
+                        int endSymbol = details.indexOf(symbol, startSymbol+1);
+
+                        if(startSymbol != -1 && endSymbol != -1) {
+                            taInfo.setText(details.substring(startSymbol + symbol.length(), endSymbol));
+                        } else {
+                            taInfo.setText("Not found!");
+                        }
+
+                    } catch (IOException io) {
+                        throw new RuntimeException(io);
+                    }
+                }
             }
         });
 
